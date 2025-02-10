4f1a19b4afc349027e5586bac1bd5f6bca4f06ca
allen <hoakaiz228241@outlook.com>
2025-02-10 11:45:33 +0800

    Add files via upload

---
+++ multi-agent/agent-supervisor/leader.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/Rovanta/rmodel/community/processor/go_code_tester"
+	"github.com/Rovanta/rmodel/processor"
+)
+
+const (
+	memKeyDemand   = "demand"
+	memKeyResponse = "response"
+	memKeyTask     = "task"
+	memKeyFeedback = "feedback"
+	memKeyDecision = "decision"
+)
+
+func LeaderProcess(b processor.BrainContext) error {
+	// if it has no task, disassemble task from demand
+	if !b.ExistMemory(memKeyTask) {
+		task := rephraseTaskFromDemand(b.GetMemory(memKeyDemand).(string))
+		_ = b.SetMemory(memKeyTask, task)
+		_ = b.SetMemory(memKeyDecision, DecisionRD)
+
+		return nil
+	}
+	switch b.GetMemory(memKeyFeedback).(string) {
+	case FeedBackRD: // feedback from RD
+		_ = b.SetMemory(memKeyDecision, DecisionQA) // pass to QA
+	case FeedBackQA: // feedback from QA
+		ok := readTestReport(b.GetMemory(memKeyGoTestResult).(string))
+		if !ok {
+			// test result not ok, resend to RD
+			_ = b.SetMemory(memKeyDecision, DecisionRD)
+		} else {
+			// pretty response from codes
+			resp := genResponse(b)
+			_ = b.SetMemory(memKeyResponse, resp)
+			_ = b.SetMemory(memKeyDecision, DecisionResponse)
+		}
+	default:
+		return fmt.Errorf("unknown feedback: %v\n", b.GetMemory(memKeyFeedback))
+	}
+
+	return nil
+}
+
+func rephraseTaskFromDemand(demand string) string {
+	// TODO maybe use completion LLM to rephrase demand to task
+	task := demand
+
+	return task
+}
+
+func readTestReport(testResult string) bool {
+	return !strings.Contains(testResult, "FAIL")
+}
+
+func genResponse(b processor.BrainContextReader) string {
+	codes := b.GetMemory(memKeyCodes).(*go_code_tester.Codes).String()
+	testReport := b.GetMemory(memKeyGoTestResult).(string)
+
+	var builder strings.Builder
+	builder.WriteString("Dear Boss:  \n")
+	builder.WriteString("After the efforts of our RD team and QA team, the final codes and test report are produced as follows:\n\n")
+	builder.WriteString("==========\n\nCodes:\n\n")
+	builder.WriteString(codes)
+	builder.WriteString("==========\n\nTest Report:\n\n")
+	builder.WriteString("```shell\n")
+	builder.WriteString(testReport)
+	builder.WriteString("```")
+	builder.WriteString("\n")
+
+	return builder.String()
+}
---
+++ multi-agent/agent-supervisor/rd.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/sashabaranov/go-openai"
+	"github.com/Rovanta/rmodel/community/processor/go_code_tester"
+	"github.com/Rovanta/rmodel/community/processor/openai_structured_output"
+	"github.com/Rovanta/rmodel/processor"
+)
+
+var (
+	memKeyCodes = (&go_code_tester.Codes{}).FunctionName()
+)
+
+func NewRDProcessor() *CoderProcessor {
+	return &CoderProcessor{
+		clientConfig: openai.DefaultConfig(os.Getenv("OPENAI_API_KEY")),
+		requestConfig: openai_structured_output.RequestConfig{
+			Model:       openai.GPT3Dot5Turbo0125,
+			Temperature: 0.7,
+			Stream:      false,
+		},
+	}
+}
+
+type CoderProcessor struct {
+	clientConfig  openai.ClientConfig
+	client        *openai.Client
+	requestConfig openai_structured_output.RequestConfig
+}
+
+func (p *CoderProcessor) Process(b processor.BrainContext) error {
+	var prompt string
+	if !b.ExistMemory(memKeyCodes) {
+		// read task, write code
+		prompt = fmt.Sprintf(`{{.%s}}`, memKeyTask)
+	} else {
+		// read task, old code and test result, write code
+		prompt = fmt.Sprintf(`{{.%s}}
+
+My code is as follows:
+
+%s
+
+test result is as follows:
+
+%s
+
+Help me correct my code.
+`, memKeyTask, b.GetMemory(memKeyCodes).(*go_code_tester.Codes).String(), memKeyGoTestResult)
+	}
+
+	structuredOutput := p.newStructuredOutputProcessor(prompt)
+	if err := structuredOutput.Process(b); err != nil {
+		return err
+	}
+
+	if err := b.SetMemory(memKeyFeedback, b.GetCurrentNeuronID()); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *CoderProcessor) Clone() processor.Processor {
+	return &CoderProcessor{
+		requestConfig: p.requestConfig,
+		clientConfig:  p.clientConfig,
+		client:        nil,
+	}
+}
+
+func (p *CoderProcessor) WithClientConfig(clientConfig openai.ClientConfig) *CoderProcessor {
+	p.clientConfig = clientConfig
+	return p
+}
+
+func (p *CoderProcessor) WithClient(client *openai.Client) *CoderProcessor {
+	p.client = client
+	return p
+}
+
+func (p *CoderProcessor) WithRequestConfig(requestConfig openai_structured_output.RequestConfig) *CoderProcessor {
+	p.requestConfig = requestConfig
+	return p
+}
+
+func (p *CoderProcessor) newStructuredOutputProcessor(prompt string) *openai_structured_output.OpenAIStructuredOutputProcessor {
+	proc := openai_structured_output.NewProcessor()
+	_ = proc.WithPromptTemplate(prompt)
+	_ = proc.WithOutputStructDefinition(go_code_tester.Codes{}, (go_code_tester.Codes{}).FunctionName(), (go_code_tester.Codes{}).FunctionDescription())
+	return proc.WithClientConfig(p.clientConfig).WithRequestConfig(p.requestConfig)
+}
